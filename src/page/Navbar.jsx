219b8639649eb0ea5c249a69250d4fae7079f838
RinadQudah <145657170+RinadQudah@users.noreply.github.com>
2023-12-20 18:44:11 +0300

    Add files via upload

---
+++ src/page/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <>
+   
+    <nav className="navbar">
+      <div className='dd'>
+        <a className='h5'href='/'>ッتعلم بابتسامة</a>
+      
+   </div>
+   
+      <a href="/">الصفحة الرئيسة</a>
+      <a href="contact">تواصلوا معنا</a>
+      <a href="signup">تسجيل</a>
+      <a href="login">دخول</a>
+
+      </nav></>
+  )
+}
+
+export default Navbar;
